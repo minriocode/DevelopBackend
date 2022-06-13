e8bb3db16e3df2c2bf886ec9e66638a2e2d2af71
mincodeopt <mincodeopt@gmail.com>
2022-06-13 14:55:57 +0600

    Website Initialized

---
+++ TheCafeRio/TheCafeRio.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheCafeRio.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
