455c20ffa0edab5ea8674f27992b6e40a42840f3
mincodeopt <mincodeopt@gmail.com>
2022-06-18 13:18:24 +0600

    Datatables Added

--- TheCafeRio/TheCafeRio.Web/Controllers/HomeController.cs
+++ TheCafeRio/TheCafeRio.Web/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace TheCafeRio.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
